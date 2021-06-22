a168d28520632c40e3331c6cc91d67d00df46df3
ankitmehra92 <ankitmehra92@users.noreply.github.com>
2021-06-22 18:59:24 +0530

    Resolved CORS

--- src/main/java/com/ankit/template/contollers/templateController.java
+++ src/main/java/com/ankit/template/contollers/templateController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class templateController 
 {
 	@Autowired
