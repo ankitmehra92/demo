f7f918553fba810cbd91894b416361a47b7ad80b
ankitmehra92 <ankitmehra92@users.noreply.github.com>
2021-06-22 20:07:28 +0530

    Updated CORS endpoints

--- src/main/java/com/ankit/template/contollers/templateController.java
+++ src/main/java/com/ankit/template/contollers/templateController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost","http://localhost:4200"})
 public class templateController 
 {
 	@Autowired
