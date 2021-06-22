a8f4908eada50629980cfb05ad4a49de5a3baa89
ankitmehra92 <ankitmehra92@users.noreply.github.com>
2021-06-22 13:15:44 +0530

    Initial commit

---
+++ src/main/java/com/ankit/template/contollers/templateController.java
@@ -0,0 +1,21 @@
+package com.ankit.template.contollers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class templateController 
+{
+	@Autowired
+	private Environment env;
+	@RequestMapping(path = "/namevar", method = RequestMethod.GET)
+	public ResponseEntity<String> getNameVar() {
+	String nameVar = env.getProperty("NAME");
+	return new ResponseEntity<String>("Hello "+nameVar, HttpStatus.OK);
+	}
+}
